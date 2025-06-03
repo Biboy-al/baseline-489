1aebebda735cfa61fd4f9d9cf793a13a85330b07
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-06-03 23:24:03 +1200

    Implemented main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,62 @@
-public class Main {
+import ec.EvolutionState;
+import ec.Evolve;
+import ec.util.Output;
+import ec.util.Parameter;
+import ec.util.ParameterDatabase;
+
+import java.security.KeyStore;
+
+public class Main extends Evolve {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        //used to store GP run info
+        EvolutionState state;
+
+        //used to load params of GP from a params file
+        ParameterDatabase params;
+
+        //Loads the parameters from java arguments
+        params = loadParameterDatabase(args);
+
+        //defines how many jobs to do from the param file.
+        //If it can't find any, init 1
+        int numJob = params.getIntWithDefault(new Parameter("jobs"), null, 1);
+
+        if (numJob < 0) Output.initialError("Jobs should be greater than 0");
+
+        for (int job = 0; job < numJob; job++) {
+
+            //returns an evolution state
+            // internally sets up the population, breeding piepline, and random generators
+
+            if (params == null) params = loadParameterDatabase(args);
+
+            state = initialize(params, job);
+            state.output.systemMessage("Job " + job);
+
+            // stores an object array with job number
+            state.job = new Object[1];
+            state.job[0] = job;
+
+            //stores command line arg for the run
+            state.runtimeArguments = args;
+
+            //info about multi job gp runs
+            //add prefixs to all output and checkpoint files
+            if (numJob >1){
+                String jobFilePrefix = "Job." + job + ".";
+                state.output.setFilePrefix(jobFilePrefix);
+                state.checkpointPrefix = jobFilePrefix + state.checkpointPrefix;
+            }
+
+            //clean up the state
+            state.run(EvolutionState.C_STARTED_FRESH);
+            cleanup(state);
+            params = null;
+
+
+        }
+
+
     }
 }
--- src/baseline/gp/MainGP.java
+++ src/baseline/gp/MainGP.java
@@ -1,4 +1,15 @@
 package baseline.gp;
 
-public class MainGP  {
+import ec.EvolutionState;
+import ec.Individual;
+import ec.gp.GPProblem;
+import ec.simple.SimpleProblemForm;
+
+public class MainGP extends GPProblem implements SimpleProblemForm {
+
+
+
+    public void evaluate(EvolutionState evolutionState, Individual individual, int i, int i1) {
+
+    }
 }
