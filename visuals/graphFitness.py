7ea842efa7d63df34d8c3dac8c1ef0bbe4bd3c2f
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-06-24 22:56:11 +1200

    add python graphing script

---
+++ visuals/graphFitness.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+stat = pd.read_csv("../src/baseline/outtabular.stat", sep=" ")
+
+print(stat)
+
+plt.figure(figsize=(10,6), )
+
+# plt.plot(stat["generation"], stat["bestOfGenFitness"])
+
+plt.show()
